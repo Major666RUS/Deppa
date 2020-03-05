01f1e4a10ae53ccb093617f7b9641136260238fe
Major666RUS <vadfalin666@yahoo.com>
2020-03-06 01:11:50 +0300

    fixes

--- assets/build/js/main.js
+++ assets/build/js/main.js
@@ -18443,9 +18443,18 @@ $(function(){
     $('.searchBar_search__desktop').toggle();
     if ($(event.target).is('.js-search')) {
       $('.searchBar_close').css('display', 'flex');
+      $('.js-searchInput').focus();
     }
     else {
       $('.searchBar_close').hide();
+    }
+  });
+  $('.searchBar_navItem__hasChildren').click(function(event) {
+    if ($(this).hasClass('active')) {
+      $(this).removeClass('active');
+    } else {
+      $('.searchBar_navItem__hasChildren').removeClass('active');
+      $(this).addClass('active');
     }
   });
 })
