a8371ca0665aa45ce442fd84459664c0a7240df4
Major666RUS <vadfalin666@yahoo.com>
2020-03-07 20:42:02 +0300

    searchBar

--- assets/build/js/main.js
+++ assets/build/js/main.js
@@ -18449,14 +18449,29 @@ $(function(){
       $('.searchBar_close').hide();
     }
   });
-  $('.searchBar_navItem__hasChildren').click(function(event) {
-    if ($(this).hasClass('active')) {
-      $(this).removeClass('active');
+  $('.js-catalog').click(function(event) {
+    if ($(this).parent().hasClass('active')) {
+      $(this).parent().removeClass('active');
+      $(this).next('.searchBar_catalogWrapper').slideUp();
     } else {
       $('.searchBar_navItem__hasChildren').removeClass('active');
-      $(this).addClass('active');
+      if ($('.searchBar_catalogWrapper:visible').length) {
+        var _this = $(this);
+        $('.searchBar_catalogWrapper:visible').slideUp(400, function() {
+          _this.parent().addClass('active');
+          _this.next('.searchBar_catalogWrapper').slideToggle();
+        });
+      } else {
+        $(this).parent().addClass('active');
+        $(this).next('.searchBar_catalogWrapper').slideToggle();
+      }
     }
   });
+  $('.searchBar_devicesBrand').click(function() {
+    $('.searchBar_devicesBrand').removeClass('active');
+    $('.searchBar_devicesList').removeClass('active');
+    $('[data-id=' + $(this).attr('data-id') + ']').addClass('active');
+  })
 })
 $(function(){
   $('.js-menuToggle, .mainMenu_overlay').click(function(){
